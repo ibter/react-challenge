e692c8c52e73586c020d46866b9a2500c38e5cf0
imevasoldi <imevasoldi@gmail.com>
2022-05-12 09:22:43 -0500

    to branche useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {useEffect} from "react"
 import ReactDOM from "react-dom"
 import Navbar from "./Navbar.js"
 import Main from "./Main.js"
@@ -10,17 +11,17 @@ export default function App (){
   const [comments, setComments] = React.useState([])
 
   // const [localComments, setLocalComments] = React.useState(data)
- 
+
   const [currentComment, setCurrentComment] = React.useState(0)
 
-  React.useEffect(() => {
+  useEffect(() => {
     console.log("effect ran")
     fetch("https://raw.githubusercontent.com/Bernabe-Felix/Bellotero/master/page1.json")
     .then(response => response.json())
     //.then(data => console.log(data))
     .then(data => setComments(data.slider.reviews))
   } ,[])
-  
+
   function handleNext(){
       console.log("Next cliked", currentComment)
       if(currentComment >= 0 && currentComment < comments.length - 1){
@@ -49,20 +50,20 @@ export default function App (){
     <div>
       <Navbar />
       <Main title="Our custumers love us"/>
-      {/* <Testimonials 
+      {/* <Testimonials
           name={localComments[currentComment].name}
           position={localComments[currentComment].position}
           comment={localComments[currentComment].comment}
           handleNext={handleNext}
           handlePrev={handlePrev}
       /> */}
-      {comments.length && <Testimonials 
+      {comments.length && <Testimonials
           name={comments[currentComment].name}
           position={comments[currentComment].position}
           comment={comments[currentComment].comment}
           handleNext={handleNext}
           handlePrev={handlePrev}
-      />}                      
+      />}
       {/* <Testimonials />                     */}
     </div>
   )
